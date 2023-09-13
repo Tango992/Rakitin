5a3fce4e93d81d7c226b94f9c05bca9daad608fc
Tango992 <daniel.rahmanto@gmail.com>
2023-09-13 16:02:29 +0700

    added typed.js on index

---
+++ assets/js/script.js
@@ -0,0 +1,7 @@
+var typed = new Typed('#element', {
+    strings: ['bingung?', 'gak ada waktu?', 'males keluar rumah?', 'gak ngerti cara rakitnya?'],
+    typeSpeed: 50,
+    backSpeed: 50,
+    backDelay: 1000,
+    loop: true
+});
